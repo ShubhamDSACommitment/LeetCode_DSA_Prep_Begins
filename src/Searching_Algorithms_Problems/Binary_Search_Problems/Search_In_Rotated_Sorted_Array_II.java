aeff58a12fb2a538657d36ed2d0e4115905cd706
shubhamyadav <shubhamyadav.cdac11@gmail.com>
2025-02-21 19:30:01 +0530

    CHANGE IN Search_In_Rotated_Sorted_Array_II

--- src/Searching_Algorithms_Problems/Binary_Search_Problems/Search_In_Rotated_Sorted_Array_II.java
+++ src/Searching_Algorithms_Problems/Binary_Search_Problems/Search_In_Rotated_Sorted_Array_II.java
@@ -6,27 +6,26 @@ class Search_In_Rotated_Sorted_Array_II {
         System.out.println(searchInARotatedSortedArrayII(arr,1));
     }
     public static boolean searchInARotatedSortedArrayII(int[] arr, int target) {
-      int start = 0;
-      int end = arr.length - 1;
-      while(start<=end){
-          int mid = start + (end - start) / 2;
-          if(arr[mid] == target) return true;
-          else if(arr[start]==arr[mid] && arr[mid]==arr[end]){
-              if(arr[mid]!=target){
-              start++;
-              end--;
-              }
-          }
-          else if(arr[start]<arr[mid]){
-              if(arr[mid]>=arr[start] && arr[mid]<=arr[end]) {
-                  end = mid - 1;
-              }else start = mid + 1;
-          }else{
-              if(arr[mid]<=arr[end] && arr[mid]>arr[start]){
-                  start = mid + 1;
-              }else end = mid - 1;
-          }
-      }
-      return false;
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<=end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid]==target) return true;
+            else if(arr[start]==arr[mid] && arr[mid]==arr[end]){
+                if(arr[mid]!=target){
+                    start++;
+                    end--;
+                }
+            }
+            else if(arr[start]<=arr[mid]){
+                if(target>=arr[start] && target<=arr[mid]) end = mid-1;
+                else start = mid + 1;
+            }else{
+                if(target<=arr[end] && target>=arr[mid]) start=mid+1;
+                else end = mid-1;
+            }
+        }
+        return false;
+
     }
 }
